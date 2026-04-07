3f32f2ec78ed7bf2c6f15442cd35f2ab624d117f
KiaraG8 <ST10477053@myemeris.edu.za>
2026-04-07 09:15:33 +0200

    commit 1

---
+++ src/main/java/com/mycompany/automated_testing2/Automated_Testing2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automated_testing2;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Automated_Testing2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
